b3f7a8e8c87aefd71ba5bdb58a0270c4fc396abd
Jesus Agustin Morales Marta <l18540431@gmail.com>
2023-04-28 09:29:13 -0600

    manejo de rutas

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('expresss');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('hola mundo!');
+    res.send('una Api basica desde Express');
 });
-
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la API');
+});
+app.get('/despedida', function(req, res){
+    res.send('adios desde la API');
+});
 app.listen(3000, function(){
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
